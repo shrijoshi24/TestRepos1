d24b3ed2668d64dac680049e814ecd60566bf65a
Shrikant Joshi <Shrikant.Joshi@sas.com>
2017-04-01 12:17:42 -0400

    cherry pick

--- model/MyModel.java
+++ model/MyModel.java
@@ -4,4 +4,6 @@ public static void main(String arg[])
 {
     System.out.println("Hello, I am the model class.");
     System.out.println("test a p branch.");
+
+    System.out.println("Test cherry picking");
 }
