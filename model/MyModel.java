3a0c87c404d1a0d66461ae5000c82b2f0de02568
Shrikant Joshi <Shrikant.Joshi@sas.com>
2017-04-01 12:08:14 -0400

    Revert "New sys out."
    This reverts commit 313e4a910af8e7ba6e813fb0554b6a3ee5947ec1.

--- model/MyModel.java
+++ model/MyModel.java
@@ -2,6 +2,5 @@
 
 public static void main(String arg[])
 {
-    System.out.println("Hello, I am the model class.");
-    System.out.println("Hello, I also contain the main method.");
+	System.out.println("Hello, I am the model class.");
 }
